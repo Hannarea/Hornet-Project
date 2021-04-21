3a1d08b699e3bed208ed9261397f2ed853cb20b9
Hanna <hreed3@knights.ucf.edu>
2021-04-21 00:48:00 -0400

    Update readData file

--- readData.py
+++ readData.py
@@ -49,5 +49,62 @@ def convert_to_array(data):
     return np.array(data)
 
 b = convert_to_array(a)
+print(a)
 print(b)
+print(a.shape)
+print(b.shape)
 
+
+def convert_binary(data, col, true):
+    '''
+    Converts data entries with binary answers in string format to 0's and 1's
+
+    Parameters
+    ----------
+    data : the matrix of elements
+    col : The column we are converting to 0's and 1's (first column is 0)
+    true : the string that is to be converted into a 1. All others will become a 0
+
+    Returns
+    -------
+    the altered matrix
+
+    '''
+    
+    for i in range(len(data)):
+        if (data[i,col] == true):
+            data[i,col] = 1
+        else:
+            data[i,col] = 0
+            
+    return data
+
+c = convert_binary(b, 2, 'yes')
+print(c)
+
+def select_data(data, col, keep):
+    '''
+    reduces the data besed on the 'keep' values in the specified column
+
+    Parameters
+    ----------
+    data : matix of original data
+    col : the column to look at during the selection process, first column is 0
+    keep : the values in the column to keep, array type
+
+    Returns
+    -------
+    new matrix
+
+    '''
+    i=0
+    while (i < len(data)):
+        if data[i,col] not in keep:
+            data = np.delete(data, i, 0)
+            print('deleting row', i)
+        i+=1
+    return data
+
+keep = ['red', 'blue']
+d = select_data(c, 1, keep)
+print(d)
