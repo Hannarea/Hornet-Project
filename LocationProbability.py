2c560e0e22e07ce40f9882c2a3470a011cce91fe
Hanna <hreed3@knights.ucf.edu>
2021-04-21 22:51:00 -0400

    Updated location probability file. I think it is complete!

--- LocationProbability.py
+++ LocationProbability.py
@@ -6,40 +6,13 @@ Created on Mon Apr  5 17:00:02 2021
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # This is intended to be used as a module from which location_probability can be called. 
 
 
-def build_location_probabilities_column(data):
-    '''
-    build a column of location probabilities for the data used to train the model
-    assumed that the data is in chronological order with row 0 being oldest case on 9/19/2019
 
-    Parameters
-    ----------
-    data : an matrix of previous sightings location infomation- given in format
-            [lab status, latitude, longitude]
 
-    Returns
-    -------
-    a vector of the location probabilities for the data to train the model 
-
-    '''
-    # This is the first positive case in the data! date 9/19/2019
-    prior = np.array([49.1494, -123.943])
-    location_probabilities = np.array([1]) # We know that the first positive case is positive
-    
-    for i in range(len(data)): # for each row in the data
-        # Calculate the location_probability and add to our vector
-        p = location_probability(prior, convert_coordinates(data[i, 1:]))
-        location_probabilities = np.append(location_probabilities, p)
-        # add the location to priors if it was actually positive
-        if data[i, 0] == 'Positive ID':
-            prior = np.vstack([prior, data[i, 1:]])
-        # repeat!
-    return location_probabilities
-    
-    
 
 def convert_coordinates(location):
     '''
@@ -58,77 +31,6 @@ def convert_coordinates(location):
 
     
     
-
-def location_probability(prior, new):
-    """
-    Calculates the probability that a hornet is at location l given
-    the previous locations Li 
-    
-    Use the minimum distance from a previous siting as the 
-    argument into the pdf 
-
-    Parameters
-    ----------
-    prior : an np array of locations that were previously verified, each row is a new location
-    new : an np array the new location
-
-    Returns
-    -------
-    a scalar between 0 and 1
-
-    """
-    # Calculates the minimum norm 
-    d = norm(prior[:,0], new)
-    for i in len(prior[:,0]):
-        if (d > norm(prior[:,i], new)):
-            d = norm(prior[:,i], new)
-    return pdf(d)
-
-
-def add_new_location(prior, new, ID, keep = 'Positive ID'):
-    '''
-    
-    add the new location to the prior locations IF it is a positive ID 
-
-    Parameters
-    ----------
-    prior : array of prior locations that are already known
-    new : location under consideration
-    ID : the value in the column 'Lab Status'
-
-    Returns
-    -------
-    Updated prior array
-
-    '''
-    
-    if ID == keep:
-        new_row = np.array([new[0], new[1]])
-        return np.vstack([prior, new_row])
-    return prior
-
-
-def norm(x, y):
-    """
-    calculates the l2 norm 
-
-    Parameters
-    ----------
-    x : an np array
-    y : an np array
-
-    Returns
-    -------
-    a scalar, the l2 norm
-
-    """
-    if len(x) != len(y):
-        print('invalid arguments into norm. len(x) != len(y)')
-        return 0
-    
-    return np.sum(np.square(x-y))
-    
-    
 def pdf(x):
     """
     
@@ -143,9 +45,109 @@ def pdf(x):
     a number between 0 and 1.
 
     """
-    sig = 5
+    sig = .44
     return 1/sig/np.sqrt(2*np.pi)*np.exp(-1/2*(x/sig)**2)
 
+x = np.linspace(0,1,20)
+plt.plot(x, pdf(x), 'ro')
+plt.plot(.5, .5, 'bo')
+plt.show()
     
+
+def norm (x, y):
+    return np.sqrt((x[0]-y[0])**2+(x[1]-y[1])**2)
+
+def location_probability(prior, new):
+    """
+    Calculates the probability that a hornet is at location new given
+    the previous locations in prior
     
+    Use the minimum distance from a previous siting as the 
+    argument into the pdf 
+
+    Parameters
+    ----------
+    prior : an nx2 np array of locations that were previously verified, each row is a new location
+    new : an 1x2 np array the new location
+
+    Returns
+    -------
+    a scalar between 0 and 1
+
+    """
+    # Calculates the minimum norm
+    
+    d = norm(prior[0,:], new)
+    for i in range(len(prior)):
+        if (d > norm(prior[i, :], new)):
+            d = norm(prior[i, :], new)
+    return pdf(d)
+
+
+
+def build_location_probabilities_vector(data, prior = np.array([[49.1494, -123.943]])):
+    '''
+    build a column of location probabilities for the data used to train the model
+    assumed that the data is in chronological order with row 0 being oldest case
+
+    Parameters
+    ----------
+    data : an matrix of previous sighting location infomation- given in format
+            [lab status, latitude, longitude] including the sightings in prior
+    prior : an nx2 matrix of n previous locations, default is location of the first positive 
+            case in the data on 9/19/2019
+
+    Returns
+    -------
+    a vector of the location probabilities for the data 
+
+    '''
+    
+    location_probabilities = np.ones(len(prior)) # We know that the prior known cases were true
+    k = len(prior)
+    for i in range(k, len(data)): # for each row in the data
+        # Calculate the location_probability and add to our vector
+        p = location_probability(prior, convert_coordinates(data[i, 1:]))
+        print(p)
+        location_probabilities = np.append(location_probabilities, p)
+        # add the location to priors if it was actually positive
+        if data[i, 0] == 1: # 1 means 'Positive ID', we converted these already
+            prior = np.vstack([prior, data[i, 1:]])
+        # repeat!
+    return location_probabilities
+
+# Example - the red dots have high probability and the blue have low probability        
+
+prior = np.array([[0,0]])
+data = np.array([
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, -.5, .5],
+    [0, .5, -.5],
+    [0, -1, 0],
+    [0, -.2, 0],
+    [0, .2, 0],
+    [0, 1, 0],
+    [0, -.5, -.5],
+    [0, .5, -.5],
+    [0, 0, -1],
+    [0, 0, .2],
+    [0, 0, -.2],
+    [1, .5, .5],
+    [0, .7, .6], # Here, we have reds because of the true case right before
+    [0, .6, .4],
+    [0, .4, .55]
+    ])
+v = build_location_probabilities_vector(data, prior = prior)
+print(v)
+
+# the small dots show where the points like on the pdf. they should decrese as the points get farther from the origin in this example
+for i in range(len(data)):
+    if v[i] > 0.5:
+        plt.plot(data[i,1], data[i,2], 'ro')
+    else:
+        plt.plot(data[i,1], data[i,2], 'bo')
+
+    
+
     
